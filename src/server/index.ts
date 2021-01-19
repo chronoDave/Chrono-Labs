533219facbe6633d5c7d7992592a996790502644
chronoDave <davidwolters6758@gmail.com>
2021-01-19 18:19:14 +0100

    Added local remote

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import path from 'path';
 
-const port = process.env.PORT || 7777;
+const port = parseInt(process.env.PORT || '7777', 10);
+const host = process.env.NODE_ENV === 'development' ?
+  '0.0.0.0' :
+  'localhost';
 const pathClient = path.resolve(__dirname, '../client');
 
 express()
   .use(express.static(pathClient))
   .use('/', (_, res) => res.sendFile('index.html', { root: pathClient }))
-  .listen(port, () => console.log(`Listening on port: ${port}`));
+  .listen(port, host, () => console.log(`Listening on: ${host}:${port}`));
